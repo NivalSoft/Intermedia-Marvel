e2acb25164daed1c167fc01863d4d1a00b06f95d
Nicolas Famularo <nicolas.famularo@gmail.com>
2024-02-21 10:07:04 -0300

    Initial Commit

---
+++ Intermedia-Marvel/Intermedia_MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intermedia_MarvelApp.swift
+//  Intermedia-Marvel
+//
+//  Created by Nicolas Famularo on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Intermedia_MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
